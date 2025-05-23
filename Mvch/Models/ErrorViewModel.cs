f466171d0218cb5b8bf7dc2b1c718b69bb541044
Student <Student@2101343284PC>
2025-05-23 10:17:38 +0300

    Добавьте файлы проекта.

---
+++ Mvch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mvch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
